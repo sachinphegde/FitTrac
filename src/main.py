46969ffe11136dfa3fcffb4adcef138b8fa66646
sachin hegde <sachinparameshwar@gmail.com>
2024-01-17 00:13:08 +0530

    setup basic strava authentication

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from strava_auth import authentication
+def main():
+    authentication()
+
+if __name__ == "__main__":
+    main()
---
+++ src/strava_auth.py
@@ -0,0 +1,23 @@
+import requests
+import yaml
+import urllib
+import webbrowser
+
+
+
+def authentication():
+    with open('secrets/secrets.yml', 'r') as file:
+        secrets = yaml.safe_load(file)
+        clintid = secrets['ClientID']
+    #response = requests.get('https://www.strava.com/api/v3/athlete/', headers={'Authorization': 'Bearer ef41e29e0c140fdbc0675644013306a77755e2ba'})
+    hello = urllib.request.urlopen('http://www.strava.com/oauth/authorize?client_id=90219&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read')
+    hello1 = webbrowser.open('http://www.strava.com/oauth/authorize?client_id=90219&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read')
+    #response = requests.get('http://www.strava.com/oauth/authorize?client_id=90219&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read')
+    print(hello1)
+
+
+# curl -X POST https://www.strava.com/oauth/token \
+# 	-F client_id=YOURCLIENTID \
+# 	-F client_secret=YOURCLIENTSECRET \
+# 	-F code=AUTHORIZATIONCODE \
+# 	-F grant_type=authorization_code
