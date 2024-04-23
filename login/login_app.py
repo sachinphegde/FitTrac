63cb1dad4307ccd88fed081bdd82e7be2d57989f
sachin hegde <34891347+sachinphegde@users.noreply.github.com>
2024-04-23 22:14:04 +0530

    add basic login page

---
+++ login/login_app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# import sys
+# from flask import Flask, request, redirect, Blueprint
+# import requests
+
+# # login = Blueprint('auth', __name__, url_prefix='/login')
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask
 from auth.auth_app import auth
+#from login.login_app import login
 
 app = Flask(__name__)
 
 app.register_blueprint(auth)
+#login.register_blueprint(login)
 
 if __name__ == '__main__':
     app.run(debug=True)
